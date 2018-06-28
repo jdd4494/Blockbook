4f7a973a8c7ac74dda803dfcbb8dd0e883e6b310
Justin Dambra <justin.dambra@workinman.com>
2018-06-27 22:18:08 -0400

    push test project

--- contracts/ConvertLib.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.18;
-
-library ConvertLib{
-	function convert(uint256 amount,uint256 conversionRate) returns (uint256 convertedAmount)
-	{
-		return amount * conversionRate;
-	}
-}
--- truffle-config.js
+++ truffle-config.js
@@ -2,24 +2,26 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 
 var infura_apikey = "WXY3CSikGF7psACSRfyF";
 var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-var address = "0x627306090abab3a6e1400e9345bc60c78a8bef57";
+var address = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57"
 
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   solc: {
-      optimizer: {
-          enabled: true,
-          runs: 200
-      }
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
   },
   networks: {
     development: {
-        host: "127.0.0.1",
-        port: 7545,
-        network_id: "*" // Match any network id
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     },
     ropsten: {
       provider: function() {
-          return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey)
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey)
       },
       network_id: 3,
       from: address,
